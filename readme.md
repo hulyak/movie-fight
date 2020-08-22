20d4973df0bdd0883337fd03c93168e4f3190da2
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-22 16:13:49 -0700

    change file structure

--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 User searches a movie on the right and left side of the app and dropdown appears, user's choice is updated in the search input. User can see the movie's summary info, also a comparison for the movies. The higher values are highlighted in blue, and lower values are highlighted in yellow.
 
 Project uses [omdbapi](http://www.omdbapi.com/)
-![project image](movie.png)
+![project image](./public/movie.png)
 
 
   
--- src/autocomplete.js
+++ src/autocomplete.js
@@ -19,52 +19,54 @@ const createAutoComplete = ({
 `;
   // <a href="#" class="dropdown-item"> between results
 
-  const input = root.querySelector('input');
-  const dropdown = root.querySelector('.dropdown');
-  const resultsWrapper = root.querySelector('.results');
+  const input = root.querySelector ('input');
+  const dropdown = root.querySelector ('.dropdown');
+  const resultsWrapper = root.querySelector ('.results');
 
-  const onInput = async (event) => {
-    const items = await fetchData(event.target.value);
+  const onInput = async event => {
+    const items = await fetchData (event.target.value);
 
     // close the menu, if nothing is searched
     if (!items.length) {
-      dropdown.classList.remove('is-active');
+      dropdown.classList.remove ('is-active');
       return;
     }
 
     resultsWrapper.innerHTML = ''; //empty search after one search
 
-    dropdown.classList.add('is-active'); //open up the menu
+    dropdown.classList.add ('is-active'); //open up the menu
     // render movies
     for (let item of items) {
-      const option = document.createElement('a');
+      const option = document.createElement ('a');
 
-      option.classList.add('dropdown-item');
+      option.classList.add ('dropdown-item');
       // multi line ``;
-      option.innerHTML = renderOption(item);
-      option.addEventListener('click', () => {
+      option.innerHTML = renderOption (item);
+      option.addEventListener ('click', () => {
         //close the dropdown
-        dropdown.classList.remove('is-active');
+        dropdown.classList.remove ('is-active');
         // update the value of the input
-        input.value = inputValue(item);
+        input.value = inputValue (item);
         //helper function for second api
-        onOptionSelect(item);
+        onOptionSelect (item);
       });
-      resultsWrapper.appendChild(option);
+      resultsWrapper.appendChild (option);
     }
   };
 
-  input.addEventListener('input', debounce(onInput, 500));
+  input.addEventListener ('input', debounce (onInput, 500));
   //input event triggers for onChange
 
   //close the menu when they click away from dropdown
   //global event listener
-  document.addEventListener('click', (event) => {
+  document.addEventListener ('click', event => {
     //events bubble up to document
     // console.log(event.target);
-    if (!root.contains(event.target)) {
+    if (!root.contains (event.target)) {
       //outside of root
-      dropdown.classList.remove('is-active');
+      dropdown.classList.remove ('is-active');
     }
   });
 };
+
+//node doesn't have dom and mocha is from node, we need to test from browser
