a59a97bc8745bb100767d90777bfb52603a8177a
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-22 13:15:12 -0700

    fake dom events with dispatchEvent

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,4 +1,6 @@
-it ('Shows an autocomplete', () => {
+// mocha hooks - execute for every test
+beforeEach (() => {
+  document.querySelector ('#target').innerHTML = ''; //clear the autocomplete
   createAutoComplete ({
     root: document.querySelector ('#target'),
     fetchData () {
@@ -13,9 +15,20 @@ it ('Shows an autocomplete', () => {
       return movie.Title;
     },
   });
+});
 
+it ('Dropdown starts closed', () => {
   //dropdown must be closed, remove is-active class
   const dropdown = document.querySelector ('.dropdown');
   // assert.strictEquals (dropdown.className, 'dropdown'); use chai
   expect (dropdown.className).not.to.include ('is-active');
+});
+
+it ('After searching, dropdown opens up', () => {
+  const input = document.querySelector ('input');
+  input.value = 'avengers';
+  input.dispatchEvent (new Event ('input')); //fake event
+
+  const dropdown = document.querySelector ('.dropdown');
+  expect (dropdown.className).to.include ('is-active');
 });
