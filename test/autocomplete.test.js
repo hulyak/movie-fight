2f5e37b14512acd91f1ae1e473330ddad7f2de47
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-22 14:51:53 -0700

    assert records fetch

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,3 +1,20 @@
+const waitFor = selector => {
+  return new Promise ((resolve, reject) => {
+    const interval = setInterval (() => {
+      if (document.querySelector (selector)) {
+        clearInterval (interval);
+        clearTimeout (timeout);
+        resolve ();
+      }
+    }, 30);
+
+    const timeout = setTimeout (() => {
+      clearInterval (interval);
+      reject ();
+    }, 2000);
+  });
+};
+
 // mocha hooks - execute for every test
 beforeEach (() => {
   document.querySelector ('#target').innerHTML = ''; //clear the autocomplete
@@ -24,11 +41,24 @@ it ('Dropdown starts closed', () => {
   expect (dropdown.className).not.to.include ('is-active');
 });
 
-it ('After searching, dropdown opens up', () => {
+it ('After searching, dropdown opens up', async () => {
   const input = document.querySelector ('input');
   input.value = 'avengers';
   input.dispatchEvent (new Event ('input')); //fake event
 
+  //delay this event, wait for dropdown-item to appear
+  await waitFor ('.dropdown-item');
+
   const dropdown = document.querySelector ('.dropdown');
   expect (dropdown.className).to.include ('is-active');
+});
+
+it ('After searching, displays some results', async () => {
+  const input = document.querySelector ('input');
+  input.value = 'avengers';
+  input.dispatchEvent (new Event ('input')); //fake event
+
+  await waitFor ('.dropdown-item');
+  const items = document.querySelectorAll ('.dropdown-item');
+  expect (items.length).to.equal (3);
 });
